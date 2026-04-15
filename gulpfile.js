fcf0840c9fc81e39630625e2e0383be5c13b3458
quocnva-09 <quocnva@runsystem.net>
2026-04-15 11:34:16 +0700

    ex4: set up scss & gulp

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const { src, dest, watch, series } = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+
+function buildStyles() {
+  return src('src/scss/**/*.scss')
+    //tạo sourcemap để tracking scss
+    .pipe(sourcemaps.init())
+    //compress scss to css
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(autoprefixer('last 2 versions'))
+    .pipe(sourcemaps.write('.'))
+    //export ra folder build/css
+    .pipe(dest('build/css'));
+}
+
+function watchTask() {
+  //thiết lập trigger thay đổi scss sang css khi có thay đổi
+  watch(['src/scss/**/*.scss'], series(buildStyles));
+}
+
+//tạo lệnh default cho npx gulp với 2 hàm truyền vào
+exports.default = series(buildStyles, watchTask);
